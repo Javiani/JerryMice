d7519d71a19632519cf820546fd2a1e15ed18d96
Eduardo Ottaviani <Edu@MacBook-Pro.local>
2016-02-27 19:00:04 -0300

    Fixing versions and minor adjustments

--- www/models/folder/list.js
+++ www/models/folder/list.js
@@ -2,7 +2,7 @@ module.exports = function(){
 
 	return {
 		items :[
-			{ text :'Eduardo', active :'active' },
+			{ text :'Lorem ipsum', active :'active' },
 			{ text :'Dapibus ac facilisis in' },
 			{ text :'Morbi leo risus' },
 			{ text :'Porta ac consectetur ac' },
