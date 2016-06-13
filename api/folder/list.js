44b5eb92354329ff3ccb76f515c9321994d2b5d8
Eduardo Ottaviani <Edu@MacBook-Pro.local>
2016-06-12 22:37:51 -0300

    Refactory, migration from ejs to nunjucks template system

--- api/folder/list.js
+++ api/folder/list.js
@@ -2,9 +2,9 @@ module.exports = function(){
 
 	return {
 		items :[
-			{ text :'Lorem ipsum', active :'active' },
+			{ text :'Lorem ipsum'},
 			{ text :'Dapibus ac facilisis in' },
-			{ text :'Morbi leo risus' },
+			{ text :'Morbi leo risus', active:'active' },
 			{ text :'Porta ac consectetur ac' },
 			{ text :'Vestibulum at eros' }
 		]
